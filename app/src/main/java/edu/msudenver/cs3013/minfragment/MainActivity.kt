1b5becc68275efc70451fdd110f1c7c4b865d432
ermiyashailu <hailuermiyas0@gmail.com>
2025-04-09 17:11:39 -0600

    update2

---
+++ app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
@@ -0,0 +1,23 @@
+package edu.msudenver.cs3013.minfragment
+//Ermiyas Hailu
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    fun startFragment(view: View) {
+        val blueFragment = BlueFragment.newInstance("FIRST")
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, blueFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
